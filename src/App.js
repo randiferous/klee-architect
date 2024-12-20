0d6ecc8e8c5fbc36db7b701e5f2e56db58cdc688
Daniel Y. Lee <daniel.younghwan.lee@gmail.com>
2024-12-19 21:06:04 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="my-4">Welcome to React with Bootstrap!</h1>
+      <button className="btn btn-primary">Bootstrap Button</button>
     </div>
   );
 }
